dc6c732536bac4f7b73567a430297fc0848aa19b
lewis-developer-33 <shadowvictor33@gmail.com>
2025-08-28 13:25:28 +0300

    port issues

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port,'0.0.0.0');
 }
 bootstrap();
