224d509c203b1a303127771ddc280aab2e57e9a1
lewis-developer-33 <shadowvictor33@gmail.com>
2025-08-28 12:55:03 +0300

    duplicates removed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,10 @@ import { ChatAiModule } from './chat-ai/chat-ai.module';
 import { PayoutsModule } from './payouts/payouts.module';
 import { PaymentsModule } from './payments/payments.module';
 import { DisputesModule } from './disputes/disputes.module';
-import { ReviewsModule } from './reviews/reviews.module';
-import { TasksModule } from './tasks/tasks.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ReviewsModule } from './reviews/reviews.module';
-import { DisputesModule } from './disputes/disputes.module';
-import { PaymentsModule } from './payments/payments.module';
-import { PayoutsModule } from './payouts/payouts.module';
-import { UsersModule } from './users/users.module';
+
+
 
 @Module({
   imports: [UsersModule, TasksModule, ReviewsModule, DisputesModule, PaymentsModule, PayoutsModule, ChatAiModule],
